e244e910392cda782f5bae442fa45c4f6de44ad4
kajalpatil89 <kajalmukundpatil@usf.edu>
2025-06-03 12:41:14 -0700

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+document.getElementById("menu").innerHTML = `
+  <a href="index.html">Home</a>
+  <a href="aboutus.html">About Us</a>
+  <a href="managerecords.html">Manage Records</a>
+  <a href="analytics.html">Analytics</a>
+`;
